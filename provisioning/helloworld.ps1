be3210f26ce306aac7e56ebb3f57451b1fb29acb
Jennifer OConnor <jennifero@liatrio.com>
2020-03-10 18:50:12 -0400

    Added provisioning folder

---
+++ provisioning/helloworld.ps1
@@ -0,0 +1,11 @@
+# Filename: helloworld.ps1
+
+Write-Host
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+
+# Write file to user directory
+New-Item -Path C:\testing.txt -ItemType File
+
+# end of script
+
